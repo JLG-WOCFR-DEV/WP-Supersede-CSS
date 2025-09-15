de3bf7da3ee08ff60d130467c2daaadcccb82cb6
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-15 18:56:52 +0200

    Use esc_like when querying options

--- supersede-css-jlg-enhanced/src/Infra/Routes.php
+++ supersede-css-jlg-enhanced/src/Infra/Routes.php
@@ -191,7 +191,10 @@ final class Routes {
     public function exportConfig(): \WP_REST_Response {
         global $wpdb;
         $options = [];
-        $sql = $wpdb->prepare("SELECT option_name, option_value FROM {$wpdb->options} WHERE option_name LIKE %s", 'ssc_%');
+        $sql = $wpdb->prepare(
+            "SELECT option_name, option_value FROM {$wpdb->options} WHERE option_name LIKE %s",
+            \esc_like('ssc_') . '%'
+        );
         $results = $wpdb->get_results($sql);
         foreach ($results as $result) {
             $options[$result->option_name] = maybe_unserialize($result->option_value);
