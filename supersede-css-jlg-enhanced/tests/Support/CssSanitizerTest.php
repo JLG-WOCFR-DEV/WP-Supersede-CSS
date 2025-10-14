ebd8678ae06aac29ea2d19258807dda9abe7f2cb
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-14 11:58:08 +0200

    Fix phpunit warnings from reflection imports

--- supersede-css-jlg-enhanced/tests/Support/CssSanitizerTest.php
+++ supersede-css-jlg-enhanced/tests/Support/CssSanitizerTest.php
@@ -1,6 +1,5 @@
 <?php declare(strict_types=1);
 
-use ReflectionClass;
 use SSC\Support\CssSanitizer;
 
 class CssSanitizerTest extends WP_UnitTestCase
@@ -138,7 +137,7 @@ class CssSanitizerTest extends WP_UnitTestCase
 
     public function test_internal_url_sanitizer_preserves_literals(): void
     {
-        $reflection = new ReflectionClass(CssSanitizer::class);
+        $reflection = new \ReflectionClass(CssSanitizer::class);
         $sanitizeUrls = $reflection->getMethod('sanitizeUrls');
         $sanitizeUrls->setAccessible(true);
 
@@ -182,7 +181,7 @@ class CssSanitizerTest extends WP_UnitTestCase
 
     public function test_internal_import_sanitizer_preserves_literals(): void
     {
-        $reflection = new ReflectionClass(CssSanitizer::class);
+        $reflection = new \ReflectionClass(CssSanitizer::class);
         $sanitizeImports = $reflection->getMethod('sanitizeImports');
         $sanitizeImports->setAccessible(true);
 
